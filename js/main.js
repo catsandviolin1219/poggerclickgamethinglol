0e8b64c158c9c8f9b2f1359b63b7a0906b96cce1
MusicByCameron <jcbrian1118@gmail.com>
2021-12-30 17:54:49 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let poggerpoints = 0;
-let inventory = [0, 0]
-
+let inventory = [0, 0];
+let repeatedLoopIID = 0; // interval id
+let poggerMultiplyScale = 1; // for double money
+let defaultPoggerMultiplyScale = 1;
+let doubleMoneyCost = 250;
 function openShop() {
     document.getElementById("shop").style.display = "inline";
 }
@@ -10,13 +13,22 @@ function closeShop() {
 }
 
 function updateUI() {
-    document.getElementById("poggerPointsText").innerHTML = "You have " + poggerpoints.toString() + " Pogger Points";
+    document.getElementById("doubleCost").innerHTML = "Cost: " + doubleMoneyCost + " Pogger Points"
+    document.getElementById("poggerPointsText").innerHTML = "You have " + poggerpoints.toString() + " Pogger Points (&Oslash;)";
     document.getElementById("poggersAmount").innerHTML = "You have: " + inventory[0].toString();
     document.getElementById("potionsAmount").innerHTML = "You have: " + inventory[1].toString();
+    document.getElementById("debug-inventory").innerHTML = inventory.toString();
+    document.getElementById("debug-interval-id").innerHTML = repeatedLoopIID.toString();
+    if (poggerMultiplyScale != 1) {
+        document.getElementById("doubleMoneyActive").innerHTML = "Currently Active (multiplier x" + poggerMultiplyScale.toString() + ")";
+    }
+    else {
+        document.getElementById("doubleMoneyActive").innerHTML = "Currently Inactive";
+    }
 }
 
 function getPogger(amountToReward) {
-    poggerpoints += amountToReward;
+    poggerpoints += amountToReward * poggerMultiplyScale;
     updateUI();
 }
 
@@ -39,3 +51,34 @@ function buyItem(cost, itemId) {
     }
 }
 
+function activateDoubleMoney() {
+    if (doubleMoneyCost > poggerpoints) {
+        alert("NO T ENO UG H MON EY >:(");
+    }
+    else {
+        poggerpoints -= doubleMoneyCost;
+        poggerMultiplyScale = poggerMultiplyScale * 2
+        doubleMoneyCost = 250 * poggerMultiplyScale;
+        updateUI();
+        setTimeout(function () {
+            poggerMultiplyScale = poggerMultiplyScale / 2
+            doubleMoneyCost = 250 * poggerMultiplyScale;
+            updateUI();
+        }, 30000)
+    }
+}
+
+function repeatedLoop() {
+    getPogger(
+        (inventory[0] * 1) +
+        (inventory[1] * 5)
+    );
+    updateUI();  
+}
+
+function main() {
+    updateUI();
+    repeatedLoopIID = setInterval("repeatedLoop()", 1000);
+    document.getElementById("debug-inventory").innerHTML = inventory.toString();
+    document.getElementById("debug-interval-id").innerHTML = repeatedLoopIID.toString();
+}
